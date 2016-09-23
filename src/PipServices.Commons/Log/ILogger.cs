2710be282ae1f16b6be008b3df1c8680760f0222
vovant <vovantigo@gmail.com>
2016-09-23 13:54:31 +0300

    IDescriptable, ILogger and LogLevel enum were added to solution

---
+++ src/PipServices.Commons/Log/ILogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PipServices.Commons.Log
+{
+    public interface ILogger
+    {
+        LogLevel Level { get; set; }
+
+        void Write(string correlationId, LogLevel level, Exception error, string message, params object[] args);
+
+        void Fatal(string correlationId, string message, params object[] args);
+        void Fatal(string correlationId, Exception error);
+        void Fatal(string correlationId, Exception error, string message, params object[] args);
+
+        void Error(string correlationId, string message, params object[] args);
+        void Error(string correlationId, Exception error);
+        void Error(string correlationId, Exception error, string message, params object[] args);
+
+        void Warn(string correlationId, string message, params object[] args);
+
+        void Info(string correlationId, string message, params object[] args);
+
+        void Debug(string correlationId, string message, params object[] args);
+
+        void Trace(string correlationId, string message, params object[] args);
+    }
+}
