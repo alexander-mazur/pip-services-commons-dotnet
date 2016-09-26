0195d68823eda2ec69bcda20cc0d3999aee9cc00
vovant <vovantigo@gmail.com>
2016-09-26 19:30:07 +0300

    Tests for COmposite, Debug and Event loggers were added

---
+++ PipServices.Commons.Test/Log/DebugLoggerTest.cs
@@ -0,0 +1,35 @@
+﻿using PipServices.Commons.Log;
+using Xunit;
+
+namespace PipServices.Commons.Test.Log
+{
+    public sealed class DebugLoggerTest
+    {
+        private ILogger Log { get; set; }
+        private LoggerFixture Fixture { get; set; }
+
+        public DebugLoggerTest()
+        {
+            Log = new DebugLogger();
+            Fixture = new LoggerFixture(Log);
+        }
+
+        [Fact]
+        public void TestLogLevel()
+        {
+            Fixture.TestLogLevel();
+        }
+
+        [Fact]
+        public void TestSimpleLogging()
+        {
+            Fixture.TestSimpleLogging();
+        }
+
+        [Fact]
+        public void TestErrorLogging()
+        {
+            Fixture.TestErrorLogging();
+        }
+    }
+}
---
+++ PipServices.Commons.Test/Log/EventLoggerTest.cs
@@ -0,0 +1,35 @@
+﻿using PipServices.Commons.Log;
+using Xunit;
+
+namespace PipServices.Commons.Test.Log
+{
+    public sealed class EventLoggerTest
+    {
+        private ILogger Log { get; set; }
+        private LoggerFixture Fixture { get; set; }
+
+        public EventLoggerTest()
+        {
+            Log = new EventLogger();
+            Fixture = new LoggerFixture(Log);
+        }
+
+        [Fact]
+        public void TestLogLevel()
+        {
+            Fixture.TestLogLevel();
+        }
+
+        [Fact]
+        public void TestSimpleLogging()
+        {
+            Fixture.TestSimpleLogging();
+        }
+
+        [Fact]
+        public void TestErrorLogging()
+        {
+            Fixture.TestErrorLogging();
+        }
+    }
+}
