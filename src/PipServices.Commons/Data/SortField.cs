0ae82c735529b06cfb2b0b69824838a4ed80df68
Mark Zontak <mark@zontak.me>
2016-09-26 17:29:27 -0700

    Added PropertyReflector

--- src/PipServices.Commons/Data/SortField.cs
+++ src/PipServices.Commons/Data/SortField.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace PipServices.Commons.Data
+﻿namespace PipServices.Commons.Data
 {
     public class SortField
     {
@@ -15,6 +10,5 @@ namespace PipServices.Commons.Data
             Name = name;
             Ascending = ascending;
         }
-
     }
 }
---
+++ src/PipServices.Commons/Reflect/PropertyReflector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace PipServices.Commons.Reflect
+{
+    public class PropertyReflector
+    {
+        public static object GetProperty(string obj, string name)
+        {
+            if(obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if(name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            var typeInfo = obj.GetType().GetTypeInfo();
+            return typeInfo.GetProperty(name).GetValue(obj);
+        }
+
+        public static void SetProperty(object obj, string name, object value)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            var typeInfo = obj.GetType().GetTypeInfo();
+            typeInfo.GetProperty(name).SetValue(obj, value);
+        }
+
+        public static void SetProperties(object obj, Dictionary<string, object> values)
+        {
+            if (values == null || values.Count == 0) return;
+            foreach (var key in values.Keys)
+            {
+                SetProperty(obj, key, values[key]);
+            }
+        }
+    }
+}
