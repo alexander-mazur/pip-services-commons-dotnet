44f572c2efb1c3ed7eb56410e71a8a9978d2ed62
Mark Zontak <mark@zontak.me>
2016-09-26 17:25:03 -0700

    Added SortParams

---
+++ src/PipServices.Commons/Data/SortField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PipServices.Commons.Data
+{
+    public class SortField
+    {
+        public string Name { get; set; }
+        public bool Ascending { get; set; } = true;
+
+        public SortField(string name = null, bool ascending = true)
+        {
+            Name = name;
+            Ascending = ascending;
+        }
+
+    }
+}
---
+++ src/PipServices.Commons/Data/SortParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PipServices.Commons.Data
+{
+    public class SortParams : List<SortField>
+    {
+        public SortParams(IEnumerable<SortField> fields = null)
+        {
+            if (fields != null)
+            {
+                AddRange(fields);
+            }
+        }
+    }
+}
