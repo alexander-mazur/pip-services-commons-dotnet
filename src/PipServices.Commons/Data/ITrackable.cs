1c9f26fe3e157c845eb974bc080b56487db5ae63
Mark Zontak <mark@zontak.me>
2016-09-27 13:49:46 -0700

    Missed files

---
+++ src/PipServices.Commons/Data/ITrackable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PipServices.Commons.Data
+{
+    public interface ITrackable
+    {
+        DateTime CreatedTime { get; }
+        DateTime LastChangeTime { get; }
+        bool IsDeleted { get; }
+    }
+}
