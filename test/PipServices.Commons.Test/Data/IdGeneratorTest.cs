b3a24f52acec6da63c0dd0a60b56550faca9eeb0
vovant <vovantigo@gmail.com>
2016-09-28 17:17:58 +0300

    Tests for IdGenerator were added

---
+++ test/PipServices.Commons.Test/Data/IdGeneratorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using PipServices.Commons.Data;
+using Xunit;
+
+namespace PipServices.Commons.Test.Data
+{
+    public sealed class IdGeneratorTest
+    {
+        private void TestIds(Func<string> generator, int minSize)
+        {
+            var id1 = generator();
+            Assert.NotNull(id1);
+            Assert.True(id1.Length >= minSize);
+
+            var id2 = generator();
+            Assert.NotNull(id2);
+            Assert.True(id2.Length >= minSize);
+            Assert.NotEqual(id1, id2);
+        }
+
+        [Fact]
+        public void TestShortId()
+        {
+            TestIds(IdGenerator.NextShort, 9);
+        }
+
+        [Fact]
+        public void TestLong()
+        {
+            TestIds(IdGenerator.NextLong, 32);
+        }
+    }
+}
